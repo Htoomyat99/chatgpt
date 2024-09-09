dfede82753c189f44fdd57223d581e03f12957fc
Htoomyat99 <htoomyat.20399@gmail.com>
2024-09-09 22:33:56 +0630

    chat gpt pj expo setUp

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Index = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontSize: 20 }}>Index</Text>
+    </View>
+  );
+};
+
+export default Index;
+
+const styles = StyleSheet.create({});
